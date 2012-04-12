d6b0040ee3d813f9669a5d12c503087044a19a7e
celostad <celostad@gmail.com>
2012-04-12 10:59:12 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,15 +1,15 @@
 <?php
 
-$host  = "mysql.maypet.com.br"; //endereço do seu servidor MySQL
-$database = "maypet02"; //preencha com o nome do BD que contem a tabela que criamos
-$login_db = "maypet02"; //login usado para acessar seu BD
-$senha_db = "12340980"; //senha usada para acessar seu BD
+$host  = "localhost"; //endereï¿½o do seu servidor MySQL
+$database = "db"; //preencha com o nome do BD que contem a tabela que criamos
+$login_db = "root"; //login usado para acessar seu BD
+$senha_db = ""; //senha usada para acessar seu BD
 
-// não altere mais nada abixo desta linha
+// nï¿½o altere mais nada abixo desta linha
 
 $connection = mysql_connect("$host","$login_db","$senha_db") 
-    or die ("Não foi possivel conectar ao servidor.");
+    or die ("Nï¿½o foi possivel conectar ao servidor.");
     
 $db = mysql_select_db("$database", $connection)
-    or die("Não foi possivel selecionar o banco de dados.");
+    or die("Nï¿½o foi possivel selecionar o banco de dados.");
 ?>
