bc4b59006686cec9a0981cc279de14ed47ddbea2
Marcelo Tadim <celostad@gmail.com>
2021-06-30 13:47:31 -0300

    Update conexao.php
    Alterado para mysqli

--- conexao.php
+++ conexao.php
@@ -1,15 +1,20 @@
 <?php
 
 $host  = "localhost"; //endere�o do seu servidor MySQL
-$database = "db"; //preencha com o nome do BD que contem a tabela que criamos
+$database = "petlivre"; //preencha com o nome do BD que contem a tabela que criamos
 $login_db = "root"; //login usado para acessar seu BD
 $senha_db = ""; //senha usada para acessar seu BD
 
-// n�o altere mais nada abixo desta linha
+// n�o altere mais nada abaixo desta linha
+    // 1. Create a database connection
+$connection = mysqli_connect($host,$login_db,$senha_db);
+if (!$connection) {
+    die("Database connection failed: " . mysqli_error());
+}
 
-$connection = mysql_connect("$host","$login_db","$senha_db") 
-    or die ("N�o foi possivel conectar ao servidor.");
-    
-$db = mysql_select_db("$database", $connection)
-    or die("N�o foi possivel selecionar o banco de dados.");
+// 2. Select a database to use 
+$db_select = mysqli_select_db($connection, $database);
+if (!$db_select) {
+    die("Database selection failed: " . mysqli_error());
+}
 ?>
